f9741a5101ff8236d332020903f2349faf83df8b
Robert Brown <Robert.Brown@tmctechnologies.com>
2022-10-21 09:44:23 -0400

    Workflow Updates - WIP Code Updates

--- compose/create_user.sql
+++ compose/create_user.sql
@@ -1,4 +1,9 @@
-CREATE USER `testuser`@`172.28.0.2` REQUIRE SUBJECT '/C=US/ST=WestVirginia/L=Fairmont/O=NASA/OU=ITC_KMC_Container/CN=itc-kmc.nasa.gov/emailAddress=Robert.J.Brown@nasa.gov';
-grant all privileges on sadb.* to `testuser`@`172.28.0.2`;
+CREATE USER `root`@`172.28.0.2` REQUIRE SUBJECT '/C=US/ST=WestVirginia/L=Fairmont/O=NASA/OU=ITC_KMC_Container/CN=itc-kmc.nasa.gov/emailAddress=Robert.J.Brown@nasa.gov';
+grant all privileges on sadb.* to `root`@`172.28.0.2`;
 flush privileges;
-
+CREATE USER `root`@`172.28.0.4` REQUIRE SUBJECT '/C=US/ST=WestVirginia/L=Fairmont/O=NASA/OU=ITC_KMC_Container/CN=redhat-itc-kmc.nasa.gov/emailAddress=Robert.J.Brown@nasa.gov';
+grant all privileges on sadb.* to `root`@`172.28.0.4`;
+flush privileges;
+CREATE USER `root`@`172.28.0.5` REQUIRE SUBJECT '/C=US/ST=WestVirginia/L=Fairmont/O=NASA/OU=ITC_KMC_Container/CN=debian-itc-kmc.nasa.gov/emailAddress=Robert.J.Brown@nasa.gov';
+grant all privileges on sadb.* to `root`@`172.28.0.5`;
+flush privileges;
